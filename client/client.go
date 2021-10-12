3944c5f9d4a483965e2b248ac730ca6d77568bb6
Karl Bonde Torp <kart@itu.dk>
2021-10-12 21:14:18 +0200

    first draft of client

--- client/client.go
+++ client/client.go
@@ -1,6 +1,35 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"log"
+	pb "github.com/axrez/disys-mini-project-2"
+)
 
-	
+type Participant struct {
+	Name string
+	Id   int32
+}
+
+func main() {
+}
+
+func JoinChat (c pb.ChittyChatClient, ctx context.Context, name string, lTime int32) Participant{
+	message := &pb.JoinMessage{
+		Name: name,
+		LTime: lTime,
+	}
+	_, err := c.Join(ctx, message)
+	if err != nil {
+		log.Fatalf("%s failed to join: %v", name, err)
+	}
+	return Participant{Id: 1, Name: name}
+}
+
+func Welcome() string {
+	var name string
+	fmt.Print("Please type your name: ")
+	fmt.Scanln(&name)
+	return name
 }
