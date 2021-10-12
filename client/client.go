d560484272f6e93fd12e4cbe4ef3bdf910a8ba65
Karl Bonde Torp <kart@itu.dk>
2021-10-12 21:38:07 +0200

    Implemented Join in client

--- client/client.go
+++ client/client.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"time"
 	"fmt"
 	"log"
 	pb "github.com/axrez/disys-mini-project-2"
+	"google.golang.org/grpc"
 )
 
 type Participant struct {
@@ -12,7 +14,27 @@ type Participant struct {
 	Id   int32
 }
 
+const address = "localhost:50051"
+
 func main() {
+	name := GetParticipantName()
+	var lTime int32 = 1
+
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewChittyChatClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+
+	defer cancel()
+
+	p := JoinChat(c, ctx, name, lTime)
+
+	fmt.Printf("Participant received with ID: %d and Name: %s", p.Id, p.Name)
 }
 
 func JoinChat (c pb.ChittyChatClient, ctx context.Context, name string, lTime int32) Participant{
@@ -27,7 +49,7 @@ func JoinChat (c pb.ChittyChatClient, ctx context.Context, name string, lTime in
 	return Participant{Id: r.GetId(), Name: r.GetName()}
 }
 
-func Welcome() string {
+func GetParticipantName() string {
 	var name string
 	fmt.Print("Please type your name: ")
 	fmt.Scanln(&name)
